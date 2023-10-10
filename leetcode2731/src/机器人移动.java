7c4afbe223ed37b651dfb96a8481e14b3fffaee4
zh <2545850211@qq.com>
2023-10-10 14:04:45 +0800

    1010移动机器人

---
+++ leetcode2731/src/机器人移动.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class 机器人移动 {
+    public static void main(String[] args) {
+
+    }
+    static final int MOD = 1000000007;
+
+    public int sumDistance(int[] nums, String s, int d) {
+        int n = nums.length;
+        long[] pos = new long[n];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'L') {
+                pos[i] = (long) nums[i] - d;
+            } else {
+                pos[i] = (long) nums[i] + d;
+            }
+        }
+        Arrays.sort(pos);
+        long res = 0;
+        for (int i = 1; i < n; i++) {
+            res += 1L * (pos[i] - pos[i - 1]) * i % MOD * (n - i) % MOD;
+            res %= MOD;
+        }
+        return (int) res;
+    }
+
+}
