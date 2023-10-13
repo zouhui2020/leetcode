c7ee02695a95898db47097714d9c7ac558b84c64
zh <2545850211@qq.com>
2023-10-13 13:56:19 +0800

    1013避免洪水泛滥

---
+++ leetcode1488/src/避免洪水泛滥.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class 避免洪水泛滥 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] avoidFlood(int[] rains) {
+        int[] ans = new int[rains.length];
+        Arrays.fill(ans, 1);
+        TreeSet<Integer> st = new TreeSet<Integer>();
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for (int i = 0; i < rains.length; ++i) {
+            if (rains[i] == 0) {
+                st.add(i);
+            } else {
+                ans[i] = -1;
+                if (mp.containsKey(rains[i])) {
+                    Integer it = st.ceiling(mp.get(rains[i]));
+                    if (it == null) {
+                        return new int[0];
+                    }
+                    ans[it] = rains[i];
+                    st.remove(it);
+                }
+                mp.put(rains[i], i);
+            }
+        }
+        return ans;
+    }
+}
