d6c914aacda871fc919d6fc1f023e6718bcd9d4e
zh <2545850211@qq.com>
2023-10-17 10:56:08 +0800

    1017种花问题

---
+++ leetcode605/src/种花问题.java
@@ -0,0 +1,29 @@
+public class 种花问题 {
+    public static void main(String[] args) {
+        int[] flowers = {0,0,1,0,1};
+        System.out.println(canPlaceFlowers(flowers, 1));
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int m = flowerbed.length;
+        int prev = -1;
+        for (int i = 0; i < m; i++) {
+            if (flowerbed[i] == 1) {
+                if (prev < 0) {
+                    count += i / 2;
+                } else {
+                    count += (i - prev - 2) / 2;
+                }
+                prev = i;
+            }
+        }
+        if (prev < 0) {
+            count += (m + 1) / 2;
+        } else {
+            count += (m - prev - 1) / 2;
+        }
+        return count >= n;
+    }
+
+}
