cf77c069227727effcaabe7e3d846ba90264acbb
zh <2545850211@qq.com>
2023-10-20 08:34:12 +0800

    1020根据规则将箱子分类

---
+++ leetcode2525/src/根据规则将箱子分类.java
@@ -0,0 +1,20 @@
+public class 根据规则将箱子分类 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String categorizeBox(int length, int width, int height, int mass) {
+        long maxd = Math.max(length, Math.max(width, height)), vol = 1L * length * width * height;
+        boolean isBulky = maxd >= 10000 || vol >= 1000000000, isHeavy = mass >= 100;
+        if (isBulky && isHeavy) {
+            return "Both";
+        } else if (isBulky) {
+            return "Bulky";
+        } else if (isHeavy) {
+            return "Heavy";
+        } else {
+            return "Neither";
+        }
+    }
+}
