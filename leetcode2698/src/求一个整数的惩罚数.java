e30379be7265cc4ae788f6d878475955f997aee8
zh <2545850211@qq.com>
2023-10-25 14:01:54 +0800

    1024求一个整数的惩罚数，回溯算法

---
+++ leetcode2698/src/求一个整数的惩罚数.java
@@ -0,0 +1,33 @@
+public class 求一个整数的惩罚数 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int punishmentNumber(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            String s = Integer.toString(i * i);
+            if (dfs(s, 0, 0, i)) {
+                res += i * i;
+            }
+        }
+        return res;
+    }
+
+    public static boolean dfs(String s, int pos, int tot, int target) {
+        if (pos == s.length()) {
+            return tot == target;
+        }
+        int sum = 0;
+        for (int i = pos; i < s.length(); i++) {
+            sum = sum * 10 + s.charAt(i) - '0';
+            if (sum + tot > target) {
+                break;
+            }
+            if (dfs(s, i + 1, sum + tot, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
