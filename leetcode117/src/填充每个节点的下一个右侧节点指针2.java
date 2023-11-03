4a5af6767c4d7213cd86c656c50a0e3e4841a1e4
zh <2545850211@qq.com>
2023-11-03 13:51:59 +0800

    1103填充每个节点的下一个右侧节点指针2

---
+++ leetcode117/src/填充每个节点的下一个右侧节点指针2.java
@@ -0,0 +1,53 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class 填充每个节点的下一个右侧节点指针2 {
+    public static void main(String[] args) {
+
+    }
+    public static Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new ArrayDeque<Node>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 1; i <= n; ++i) {
+                Node f = queue.poll();
+                if (f.left != null) {
+                    queue.offer(f.left);
+                }
+                if (f.right != null) {
+                    queue.offer(f.right);
+                }
+                if (i != 1) {
+                    last.next = f;
+                }
+                last = f;
+            }
+        }
+        return root;
+    }
+     static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+}
+
