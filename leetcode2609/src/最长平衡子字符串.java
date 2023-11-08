32cd9122ca13d531c486c8bb0e8df557464d4a62
zh <2545850211@qq.com>
2023-11-08 15:26:37 +0800

    1108最长平衡子字符串

---
+++ leetcode2609/src/最长平衡子字符串.java
@@ -0,0 +1,23 @@
+public class 最长平衡子字符串 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int findTheLongestBalancedSubstring(String s) {
+        int res = 0;
+        int n = s.length();
+        int[] count = new int[2];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                count[1]++;
+                res = Math.max(res, 2 * Math.min(count[0], count[1]));
+            } else if (i == 0 || s.charAt(i - 1) == '1') {
+                count[0] = 1;
+                count[1] = 0;
+            } else {
+                count[0]++;
+            }
+        }
+        return res;
+    }
+}
