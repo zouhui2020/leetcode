57cf45201a1104c3f69ce7500a1bbc9ff28faab7
zh <2545850211@qq.com>
2023-11-10 09:43:18 +0800

    1110咒语和药水成功对数

---
+++ leetcode2300/src/咒语和药水成功对数.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class 咒语和药水成功对数 {
+    public static void main(String[] args) {
+
+    }
+    public static int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length, m = potions.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            long t = (success + spells[i] - 1) / spells[i] - 1;
+            res[i] = m - binarySearch(potions, 0, m - 1, t);
+        }
+        return res;
+    }
+
+    public static int binarySearch(int[] arr, int lo, int hi, long target) {
+        int res = hi + 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] > target) {
+                res = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return res;
+    }
+}
