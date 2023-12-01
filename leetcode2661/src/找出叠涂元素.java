93df84ce2be47ccda754d188ff26b4961d0a07ed
zouhui <2545850211@qq.com>
2023-12-01 09:47:32 +0800

    1201找出叠涂元素

---
+++ leetcode2661/src/找出叠涂元素.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class 找出叠涂元素 {
+    public static int firstCompleteIndex(int[] arr, int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        Map<Integer, int[]> map = new HashMap<Integer, int[]>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                map.put(mat[i][j], new int[]{i, j});
+            }
+        }
+        int[] rowCnt = new int[n];
+        int[] colCnt = new int[m];
+        for (int i = 0; i < arr.length; ++i) {
+            int[] v = map.get(arr[i]);
+            ++rowCnt[v[0]];
+            if (rowCnt[v[0]] == m) {
+                return i;
+            }
+            ++colCnt[v[1]];
+            if (colCnt[v[1]] == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
