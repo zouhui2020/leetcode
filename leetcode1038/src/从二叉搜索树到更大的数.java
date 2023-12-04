a442e1587d6121f5c503b25c1ab40c9ba52c30e5
zouhui <2545850211@qq.com>
2023-12-04 14:06:20 +0800

    1204从二叉搜索树到更大的数

---
+++ leetcode1038/src/从二叉搜索树到更大的数.java
@@ -0,0 +1,34 @@
+public class 从二叉搜索树到更大的数 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+
+}
