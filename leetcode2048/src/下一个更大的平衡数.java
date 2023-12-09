9c824b1e9a752257fb682184279743b7d681583a
zouhui <2545850211@qq.com>
2023-12-09 16:26:13 +0800

    1209下一个更大的平衡数

---
+++ leetcode2048/src/下一个更大的平衡数.java
@@ -0,0 +1,24 @@
+public class 下一个更大的平衡数 {
+    public int nextBeautifulNumber(int n) {
+        for (int i = n + 1; i <= 1224444; ++i) {
+            if (isBalance(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isBalance(int x) {
+        int[] count = new int[10];
+        while (x > 0) {
+            count[x % 10]++;
+            x /= 10;
+        }
+        for (int d = 0; d < 10; ++d) {
+            if (count[d] > 0 && count[d] != d) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
