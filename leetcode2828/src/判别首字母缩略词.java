b9785a3e1d385a72659ba6e871e59995dd7b1499
zouhui <2545850211@qq.com>
2023-12-20 09:00:33 +0800

    1220判别首字母缩略词

---
+++ leetcode2828/src/判别首字母缩略词.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class 判别首字母缩略词 {
+    public boolean isAcronym(List<String> words, String s) {
+        if (words.size() != s.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (words.get(i).charAt(0) != s.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
