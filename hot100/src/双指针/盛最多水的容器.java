7e188113a69da3a1cbf51f1a21c9632ef5154207
zouhui <2545850211@qq.com>
2023-12-21 10:14:56 +0800

    hot006盛最多水的容器

---
+++ hot100/src/双指针/盛最多水的容器.java
@@ -0,0 +1,13 @@
+package 双指针;
+
+public class 盛最多水的容器 {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, res = 0;
+        while(i < j) {
+            res = height[i] < height[j] ?
+                    Math.max(res, (j - i) * height[i++]):
+                    Math.max(res, (j - i) * height[j--]);
+        }
+        return res;
+    }
+}
