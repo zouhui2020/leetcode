1f6f7c35304bf148d056907e15a7cc8495e051f4
zouhui <2545850211@qq.com>
2023-12-21 09:52:54 +0800

    1221美丽塔2

---
+++ leetcode2866/src/美丽塔2.java
@@ -0,0 +1,39 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+public class 美丽塔2 {
+    public long maximumSumOfHeights(List<Integer> maxHeights) {
+        int n = maxHeights.size();
+        long res = 0;
+        long[] prefix = new long[n];
+        long[] suffix = new long[n];
+        Deque<Integer> stack1 = new ArrayDeque<Integer>();
+        Deque<Integer> stack2 = new ArrayDeque<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack1.isEmpty() && maxHeights.get(i) < maxHeights.get(stack1.peek())) {
+                stack1.pop();
+            }
+            if (stack1.isEmpty()) {
+                prefix[i] = (long) (i + 1) * maxHeights.get(i);
+            } else {
+                prefix[i] = prefix[stack1.peek()] + (long) (i - stack1.peek()) * maxHeights.get(i);
+            }
+            stack1.push(i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack2.isEmpty() && maxHeights.get(i) < maxHeights.get(stack2.peek())) {
+                stack2.pop();
+            }
+            if (stack2.isEmpty()) {
+                suffix[i] = (long) (n - i) * maxHeights.get(i);
+            } else {
+                suffix[i] = suffix[stack2.peek()] + (long) (stack2.peek() - i) * maxHeights.get(i);
+            }
+            stack2.push(i);
+            res = Math.max(res, prefix[i] + suffix[i] - maxHeights.get(i));
+        }
+        return res;
+    }
+}
