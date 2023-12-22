42c66b06211f671f178c71ed2661419b1185984b
zouhui <2545850211@qq.com>
2023-12-22 14:14:54 +0800

    hot007无重复字符最长字串

---
+++ hot100/src/滑动窗口/无重复字符的最长字串.java
@@ -0,0 +1,18 @@
+package 滑动窗口;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 无重复字符的最长字串 {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int i = -1, res = 0, len = s.length();
+        for(int j = 0; j < len; j++) {
+            if (dic.containsKey(s.charAt(j)))
+                i = Math.max(i, dic.get(s.charAt(j))); // 更新左指针 i
+            dic.put(s.charAt(j), j); // 哈希表记录
+            res = Math.max(res, j - i); // 更新结果
+        }
+        return res;
+    }
+}
