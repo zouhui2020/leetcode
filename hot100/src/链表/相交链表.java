84ea1c6a48f87b6506859f274323a04f667e1046
zouhui <2545850211@qq.com>
2023-12-26 15:03:41 +0800

    hot018相交链表

---
+++ hot100/src/链表/相交链表.java
@@ -0,0 +1,32 @@
+package 链表;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 相交链表 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
