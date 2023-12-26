b8d6da3badd3589e84363f325737ac34c75991f0
zouhui <2545850211@qq.com>
2023-12-26 15:16:12 +0800

    hot019反转链表

---
+++ hot100/src/链表/反转链表.java
@@ -0,0 +1,33 @@
+package 链表;
+
+public class 反转链表 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+
