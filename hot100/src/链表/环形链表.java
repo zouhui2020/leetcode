26fd8f9f97baae0c67e62ec6525dc25ed4859a2d
zouhui <2545850211@qq.com>
2023-12-27 09:34:19 +0800

    hot021环形链表

---
+++ hot100/src/链表/环形链表.java
@@ -0,0 +1,27 @@
+package 链表;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 环形链表 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<ListNode>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
