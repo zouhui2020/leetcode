f133f594042bb7514f133a1cd494b8a6c382f76e
zouhui <2545850211@qq.com>
2023-12-27 10:32:50 +0800

    hot025删除链表中倒数第n个节点

---
+++ hot100/src/链表/删除链表中倒数第n个节点.java
@@ -0,0 +1,36 @@
+package 链表;
+
+public class 删除链表中倒数第n个节点 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; ++i) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+}
