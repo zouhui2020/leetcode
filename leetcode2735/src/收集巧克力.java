a106544a721a317a3426f0cbc1426663b7c6e0ec
zouhui <2545850211@qq.com>
2023-12-28 09:04:34 +0800

    1228收集巧克力

---
+++ leetcode2735/src/收集巧克力.java
@@ -0,0 +1,23 @@
+public class 收集巧克力 {
+    public long minCost(int[] nums, int x) {
+        int n = nums.length;
+        int[] f = new int[n];
+        System.arraycopy(nums, 0, f, 0, n);
+        long ans = getSum(f);
+        for (int k = 1; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                f[i] = Math.min(f[i], nums[(i + k) % n]);
+            }
+            ans = Math.min(ans, (long) k * x + getSum(f));
+        }
+        return ans;
+    }
+
+    public long getSum(int[] f) {
+        long sum = 0;
+        for (int num : f) {
+            sum += num;
+        }
+        return sum;
+    }
+}
