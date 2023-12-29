eed5aa892f2362ac0090f46d87afe7ebee954117
zouhui <2545850211@qq.com>
2023-12-29 09:10:22 +0800

    hot033对称二叉树

---
+++ hot100/src/二叉树/对称二叉树.java
@@ -0,0 +1,36 @@
+package 二叉树;
+
+public class 对称二叉树 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+}
