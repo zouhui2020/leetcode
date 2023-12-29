99a86576a7dc4439c5c2b066beafc233f248e8d2
zouhui <2545850211@qq.com>
2023-12-29 09:03:15 +0800

    hot031二叉树最大深度

--- hot100/src/二叉树/二叉树的最大深度.java
+++ hot100/src/二叉树/二叉树的最大深度.java
@@ -1,4 +1,32 @@
 package 二叉树;
 
 public class 二叉树的最大深度 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
 }
