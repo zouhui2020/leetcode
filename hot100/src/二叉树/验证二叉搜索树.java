91f2cf2e8dd9577a8a2e85d7e8efc3a9aa49bb20
zouhui <2545850211@qq.com>
2024-01-05 09:29:11 +0800

    hot036验证二叉搜索树

---
+++ hot100/src/二叉树/验证二叉搜索树.java
@@ -0,0 +1,36 @@
+package 二叉树;
+
+public class 验证二叉搜索树 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+}
