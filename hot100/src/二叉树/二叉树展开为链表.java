fc4ee00142231b64e8b9983c3a0e43bf89738f1f
zouhui <2545850211@qq.com>
2024-01-05 09:59:22 +0800

    hot038二叉数展开为链表

---
+++ hot100/src/二叉树/二叉树展开为链表.java
@@ -0,0 +1,45 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 二叉树展开为链表 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+}
