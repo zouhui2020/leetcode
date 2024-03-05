b878ca646f29133b3d2d1e84c23c092e5abf9e68
zouhui <2545850211@qq.com>
2024-03-05 14:32:47 +0800

    0305搜索插入位置

--- hot100/src/二分查找/搜索插入位置.java
+++ hot100/src/二分查找/搜索插入位置.java
@@ -8,19 +8,14 @@ public class 搜索插入位置 {
     }
     public static int searchInsert(int[] nums, int target) {
         int left = 0,right = nums.length-1;
-        int mid=(left + right)/2;
-        while(left<right){
-            mid=(left + right)/2;
+        while(left<=right){
+            int mid = left + (right - left)/2;
             if(target > nums[mid]){
                 left = mid + 1;
             }
-            if(target < nums[mid]){
+            else
                 right = mid - 1;
-            }
-            if(target == nums[mid]){
-                return mid;
-            }
         }
-        return mid;
+        return left;
     }
 }
